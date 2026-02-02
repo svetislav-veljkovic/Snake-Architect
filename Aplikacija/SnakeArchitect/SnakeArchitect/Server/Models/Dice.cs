1f0ca0b94827f4167982f6ac8742ae94428c5e95
Milica Micic <milica.micic30@gmail.com>
2026-02-02 20:31:21 +0100

    Ispravljen Move i dodat Dice

---
+++ Aplikacija/SnakeArchitect/SnakeArchitect/Server/Models/Dice.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models
+{
+    public class Dice
+    {
+        [Key]
+        public int ID { get; set;}
+
+        [Required]
+        public int PlayerId { get; set;}
+        public virtual Player Player { get; set;}
+        
+        [Required]
+        public int GameBoardId { get; set;}
+        public GameBoard GameBoard { get; set;}
+
+        [Required]
+        [Range(1,6)]
+        public int Value  { get; set;}
+
+        public DateTime Timestamp { get; set;}
+
+        public Dice(int playerId, int gameBoardId, int value)
+        {
+            PlayerId = playerId;
+            GameBoardId = gameBoardId;
+            Value = value;
+        }
+
+    }
+}
